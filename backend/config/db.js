2cceaf3d5a10d7c28c47f89473a341c4450bfc02
sujatapowar <sujata.powar@suyotechsolutions.com>
2025-01-30 16:43:09 +0530

    updated mongo url

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,10 @@ import mongoose from 'mongoose'
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/MERN_FOOD_DEL')
+    const uri =
+      process.env.MONGO_URI || 'mongodb://localhost:27017/MERN_FOOD_DEL'
+    // await mongoose.connect('mongodb://localhost:27017/MERN_FOOD_DEL')
+    await mongoose.connect(uri)
     console.log('DB Connected')
   } catch (error) {
     console.log('Error connecting to DB ', error.message)
